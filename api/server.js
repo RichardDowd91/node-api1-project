30ed4c725091ac61c3815428e7e8146a9e8d8a5a
Richard Dowd <richarddowd@Richards-MacBook-Pro-2.local>
2022-06-06 13:00:07 -0700

    have post and get on the server

--- api/server.js
+++ api/server.js
@@ -1,3 +1,39 @@
 // BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const express = require('express');
+const Users = require('./users/model');
+
+const server = express();
+
+server.use(express.json());
+
+//POST
+server.post('/api/users', (req,res) => {
+    const user = req.body
+
+    if (!user.name || !user.bio) {
+        res.status(400).json({ message: "Please provide name and bio for the user" })
+    } else {
+        Users.insert(user)
+          .then(newUser => {
+              res.status(201).json(newUser)
+          })
+          .catch(err => {
+              res.status(500).json({ message: "There was an error while saving the user to the database" })
+          })  
+    }
+})
+
+
+//GET
+server.get('/api/users', (req, res) => {
+    Users.find()
+      .then(users => {
+          res.json(users)
+      })
+      .catch(err => {
+          res.status(500).json({ message: "The users information could not be retrieved" })
+      })
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
