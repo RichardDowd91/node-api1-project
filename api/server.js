2c8b0a870a34573129e79f116f7178bc29831f3b
Richard Dowd <richarddowd@Richards-MacBook-Pro-2.local>
2022-06-06 13:24:21 -0700

    added all the crud features to the server

--- api/server.js
+++ api/server.js
@@ -36,4 +36,51 @@ server.get('/api/users', (req, res) => {
       })
 })
 
+//GET/:id
+server.get('/api/users/:id', (req, res) => {
+    Users.findById(req.params.id)
+      .then(user => {
+          if(!user) {
+              res.status(404).json({ message: "The user with the specified ID does not exist" })
+          } else {
+              res.json(user)
+          }
+      })  
+      .catch(err => {
+          res.status(500).json({ message: "The user information could not be retrieved" })
+      })
+})
+
+//DELETE
+server.delete('/api/users/:id', (req, res) => {
+    Users.remove(req.params.id)
+      .then(possibleUser => {
+          if(!possibleUser) {
+              res.json(404).json({ message: "The user with the specified ID does not exist" })
+          } else {
+              res.status(200).json(possibleUser)
+          }
+      })
+      .catch(err => {
+          res.status(500).json({ message: "The user could not be removed" })
+      })  
+})
+
+//PUT
+server.put('/api/users/:id', (req, res) => {
+    Users.update(req.params.id, req.body)
+      .then(possibleUser => {
+          if (!possibleUser) {
+              res.status(404).json({ message: " The user with the specified ID does not exist" })
+          } else if (!user.name || !user.bio) {
+              res.status(400).json({ message: "Please provide name and bio for the user" })
+          } else {
+              res.json(possibleUser)
+          }
+      }) 
+      .catch(err => {
+          res.status(500).json({ message: "The user informaiton could not be modified" })
+      }) 
+})
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
